dc870cf3a713532b7c3c7c85ed815ba706a63e98
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-10-14 15:13:24 +0300

    Initial commit

---
+++ nasvay/index.js
@@ -0,0 +1,65 @@
+const mongoseDB = 'сюдаимябазынапиши';
+
+var express = require('express');
+var app = express();
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var conn = mongoose.connection;
+
+var cors = require('cors');
+app.use(cors({ origin: 'http://xe124.ru:3000', credentials :  true}));
+
+mongoose.connect('mongodb://localhost:27017/vkfriends'{
+  useNewUrlParser: true, useUnifiedTopology: true
+});
+mongoose.model('users', new Schema({ name: String, lastName: String, id: Number, DickLength: Number }));
+
+app.get('/huy', function (req, res) {
+  res.send(req.query.string + 'ebaa')
+});
+
+app.get('/sort', (req, res) => {
+  console.log(name = req.query.string.split(',').map(nm => nm.trim()));
+
+  mongoose.model('users').find({
+    name
+  }, (err, data) => {
+    if (err)
+      throw err;
+
+    result = [];
+    // for (el of data)
+    //   result += `У ${el.get('name')} длина расписоса ${el.get('dlinaChlena')}, а живёт он на ${el.get('address')}. \n`
+
+    // console.log(result)
+
+    for (el of data)
+      result.push(el);
+
+    console.log(result);
+    
+    keysSorted = Object.keys(result).sort((a, b) => ( result[a].get('dlinaChlena') - result[b].get('dlinaChlena') ))
+    console.log(keysSorted);
+
+    sortedArray = [];
+    for (el of keysSorted)
+      sortedArray.push(result[el]);
+    console.log(sortedArray);
+    
+
+    sortedString = '';
+    for (el of keysSorted)
+      sortedString += `${result[el].get('dlinaChlena')}, `
+    console.log(sortedString)
+
+    thestring = '';
+    for (el of result)
+      thestring += `${el.get('dlinaChlena')}, `
+
+    res.send(thestring)
+  })
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ nasvay/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Input } from 'antd';
+import { Button } from 'antd';
+import axios from 'axios';
+
+const { TextArea } = Input;
+
+class App extends Component {
+  state = {users: []}
+  changeState(value) {
+    this.setState({ text: value })
+  }
+  appendSosi(value) {
+    axios.get('http://xe124.ru:3001/huy?string=' + value)
+      .then(data => {
+        this.changeState(data.data)
+      })
+  }
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <TextArea autoFocus onChange={e => { this.changeState(e.target.value) }} style={{maxWidth: "15rem"}} placeholder="Autosize height based on content lines" autosize />
+        <Button onClick={() => { this.appendSosi(this.state.text) }} type="primary">Primary</Button>
+        <div className="chlen">
+          <h1>{ this.state.text }</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
